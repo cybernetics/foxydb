de058de9592a621b31f2945735ebad544c2af76d
Aleksandar Zivanovic <aleksandar.zivanovic@shiftplanning.com>
2014-01-08 17:34:23 +0100

    removed  all console logs

--- controllers/query.js
+++ controllers/query.js
@@ -6,7 +6,7 @@ exports.controller = function(app, db) {
 	app.post('/api/query/execute', function(req, res) {
 		if(req.session.user) {
 			db.get("SELECT * FROM `databases` WHERE `id` = ?;",req.body.database_id , function(err, row) {
-				console.log(err, row, req.body);
+				// console.log(err, row, req.body);
 				var connection = mysql.createConnection({
 					host     : row.host,
 					port 	 : row.port,
